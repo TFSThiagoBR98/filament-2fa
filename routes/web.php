d0dc19dddd1bd97de0b03fad6d884ae8711c9c5a
Thiago França da Silva <tfsthiagobr98@outlook.com>
2022-12-09 11:05:21 -0300

    fix: reenable login route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::domain(config('filament.domain'))
-//     ->middleware(config('filament.middleware.base'))
-//     ->prefix(config('filament.path'))
-//     ->group(function () {
-//         Route::get('/two-factor-challenge', config('filament-2fa.two_factor_challenge_component_path'))
-//             ->name('filament-2fa.login');
-//     });
+Route::domain(config('filament.domain'))
+    ->middleware(config('filament.middleware.base'))
+    ->prefix(config('filament.path'))
+    ->group(function () {
+        Route::get('/two-factor-challenge', config('filament-2fa.two_factor_challenge_component_path'))
+            ->name('filament-2fa.login');
+    });
