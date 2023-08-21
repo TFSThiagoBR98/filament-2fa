94518b6e0be62aff405eefe7526987e7e2c96d2f
Thiago França da Silva <tfsthiagobr98@outlook.com>
2023-08-21 15:10:13 -0300

    fix: remove filament package references

--- src/FilamentTwoFactorProvider.php
+++ src/FilamentTwoFactorProvider.php
@@ -3,21 +3,25 @@
 namespace TFSThiagoBR98\FilamentTwoFactor;
 
 use Filament\Facades\Filament;
-use Filament\Navigation\UserMenuItem;
+use Filament\Navigation\MenuItem;
 use Filament\PluginServiceProvider;
 use Livewire\Livewire;
 use Spatie\LaravelPackageTools\Package;
 use TFSThiagoBR98\FilamentTwoFactor\Http\Livewire\Auth;
 use TFSThiagoBR98\FilamentTwoFactor\Http\Livewire\TwoFactorAuthenticationForm;
 use TFSThiagoBR98\FilamentTwoFactor\Pages\TwoFactor;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentTwoFactorProvider extends PluginServiceProvider
+class FilamentTwoFactorProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-2fa';
 
-    public function packageConfigured(Package $package): void
+    public function configurePackage(Package $package): void
     {
-        $package->hasRoute('web');
+        $package
+            ->name(name: static::$name)
+            ->hasRoute('web')
+            ->hasViews();
 
         Livewire::component(Auth\Login::getName(), Auth\Login::class);
         Livewire::component(Auth\TwoFactorChallenge::getName(), Auth\TwoFactorChallenge::class);
@@ -30,7 +34,7 @@ class FilamentTwoFactorProvider extends PluginServiceProvider
             && config('filament-2fa.show_two_factor_page_in_user_menu')) {
             Filament::serving(function () {
                 Filament::registerUserMenuItems([
-                    UserMenuItem::make()
+                    MenuItem::make()
                         ->label(__('filament-2fa::two-factor.navigation_label'))
                         ->url(TwoFactor::getUrl())
                         ->icon('heroicon-s-lock-closed'),
